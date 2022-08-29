94520552923100693e56db88b24de50a008fdd53
Ignacio Fernández <jifdesantis90@gmail.com>
2022-08-28 23:19:48 -0300

    Bug fixes. Add point anomaly detection logic.

--- python_api/app/anomalias/FMmodel.py
+++ python_api/app/anomalias/FMmodel.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 import numpy as np
 import pandas as pd
+from scipy.sparse import csc_matrix
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import make_pipeline
 
@@ -15,22 +16,35 @@ class FactorizationMachineAnomalyDetector:
     """
     Factorization Machine Anomaly Detector
     """
-    def __init__(self, window_size: int, fm_params: dict):
+    def __init__(self, target: str, anomaly_type: str, window_size: int, fm_params: dict, **kwargs):
         logger.info('Creating FM model.')
-        self.__pipe = make_pipeline([
+        if anomaly_type == 'point':
+            self.anomaly_type = anomaly_type
+            try:
+                self.threshold = kwargs['threshold']
+            except KeyError:
+                logger.error('Threshold must be provided for point anomaly detection.')
+        self.target = target
+        self.__pipe = make_pipeline(
             PreprocessingFMTransformer(window_size),
             FMRegression(**fm_params)
-        ])
+        )
 
     def train(self, X: pd.DataFrame, y: pd.Series):
         logger.info('Fitting model...')
-        self.__pipe.fit(X, y)
+        # First {window_size} targets are not used for training (sklearn pipelines won't change target data)
+        self.__pipe.fit(X, y.iloc[self.__pipe['preprocessingfmtransformer'].window_size:])
         logger.info(f'Model fitted.\n')
 
     def detect(self, observations: pd.DataFrame) -> pd.Series:
+        assert self.target in observations.columns, f'{self.target} not in observations columns.'
         logger.info('Detecting anomalies...')
-        # TODO decide logic for classification of anomalies
-        return self.__pipe.predict(observations)
+        y_true = observations[self.target].iloc[self.__pipe['preprocessingfmtransformer'].window_size:]
+        y_pred = self.__pipe.predict(observations)
+        out_series = y_true.sub(y_pred)\
+            .abs()\
+            .gt(self.threshold)
+        return out_series
 
 
 class PreprocessingFMTransformer(BaseEstimator, TransformerMixin):
@@ -42,36 +56,48 @@ class PreprocessingFMTransformer(BaseEstimator, TransformerMixin):
         logger.debug('Creating preprocessing FM transformer.')
 
     def fit(self, X: pd.DataFrame, y: pd.Series = None):
-        assert X.index.dtype == datetime, 'Data must be a pandas DataFrame with datetime index.'
+        assert isinstance(X.index, pd.DatetimeIndex), 'Data must be a pandas DataFrame with datetime index.'
+        assert len(X) >= self.window_size, 'Data must have at least {self.window_size} rows.'
         if y is not None:
-            assert y.index.dtype == datetime, 'Data must be a pandas DataFrame with datetime index.'
-            assert X.index.equals(y.index), 'Data must have the same index.'
+            assert isinstance(y.index, pd.DatetimeIndex), 'Data must be a pandas DataFrame with datetime index.'
+            assert X.index[self.window_size:].equals(y.index), 'Data must have the same index.'
             assert y.name in X.columns, 'Target must be a column of data.'
         return self
 
-    def transform(self, X: pd.Dataframe, y: pd.Series = None):
-        out_values = np.empty(((len(X) - self.window_size),
-                               X.shape[1]*self.window_size + 4))  # +4 is for date-time data
+    def transform(self, X: pd.DataFrame, y: pd.Series = None):
+        # out_values = np.empty(((len(X) - self.window_size),
+        #                        X.shape[1]*self.window_size + 4))  # +4 is for date-time data
+
+        # Process date-time data
+        out_values = split_datetime(X.index).values[self.window_size:, :]
+        # out_values = np.hstack((out_values, dt_values[self.window_size:, :]))
+
+        logger.debug(f'Transformed date values:\n'
+                     f'{out_values}\n'
+                     f'Shape: {out_values.shape}')
 
         # Process values on each time series
         for col in X:
-            out_col_values = np.hstack(col.values[i:-self.window_size+i] for i in range(self.window_size))
+            out_col_values = np.hstack((X[col].values[i:-self.window_size+i].reshape(-1, 1)
+                                        for i in range(self.window_size)))
+
             out_values = np.hstack((out_values, out_col_values))
 
-        # Process date-time data
-        dt_values = split_datetime(X.index).values
-        out_values = np.hstack((out_values, dt_values[self.window_size:, :]))
+        logger.debug(f'Transformed column values:\n'
+                     f'{out_values}\n'
+                     f'Shape: {out_col_values.shape}')
 
-        return out_values
+        # fastFM requires sparse matrix
+        return csc_matrix(out_values)
 
 
-def split_datetime(dt_series: pd.Series) -> pd.DataFrame:
+def split_datetime(dt_index: pd.DatetimeIndex) -> pd.DataFrame:
     """ Split datetime data into separate columns """
     out_df = pd.DataFrame()
-    out_df['day'] = dt_series.dt.day
-    out_df['weekday'] = dt_series.dt.weekday
-    out_df['month'] = dt_series.dt.month
-    out_df['hour'] = dt_series.dt.hour
+    out_df['day'] = dt_index.day
+    out_df['weekday'] = dt_index.weekday
+    out_df['month'] = dt_index.month
+    out_df['hour'] = dt_index.hour
 
     return out_df
 
@@ -80,6 +106,12 @@ if __name__ == '__main__':
     test_data = pd.DataFrame({'serie': np.random.randn(100)},
                              index=pd.date_range('1/1/2000', periods=100, freq='5min'))
 
-    fm_model = FactorizationMachineAnomalyDetector(window_size=5, fm_params={'n_iter': 10})
+    logger.debug(f'{test_data}')
+
+    fm_model = FactorizationMachineAnomalyDetector(anomaly_type='point',
+                                                   threshold=0.01,
+                                                   window_size=5,
+                                                   fm_params={'n_iter': 10, 'rank': 2},
+                                                   target='serie')
 
     fm_model.train(test_data, test_data['serie'])
